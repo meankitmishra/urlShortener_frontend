a9fbc021b5d4314bfdf2b5a260411980f472a4dc
meankitmishra <meankitmishra10@gmail.com>
2024-07-06 14:40:11 +0530

    finalizing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useState /*,useEffect*/} from 'react';
 import './App.css';
+import BackgroundAnimate from './BackgroundAnimate';
+import InputShortener from './InputShortener';
+import LinkResult from './LinkResult';
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  // const [message, setMessage] = useState('');
+
+  // useEffect(() => {
+  //   fetch('http://localhost:8080/api/message') // Adjust the port to match your backend
+  //     .then(response => response.text())
+  //     .then(data => setMessage(data))
+  //     .catch(error => console.error('There was an error!', error));
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <InputShortener setInputValue={setInputValue} />
+      <BackgroundAnimate />
+      <LinkResult inputValue={inputValue} />
     </div>
   );
 }
